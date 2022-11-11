b47f1967d67fc6839b050852392439a871d0c8a1
zhouxuios <39606817+zhouxuios@users.noreply.github.com>
2022-11-11 16:36:07 +0800

    路由优化,将browserRouter挂到app组件上

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css'
-import {BrowserRouter} from 'react-router-dom'
 import Router from './router';
 
 function App () {
   return (
     <div className='app'>
-      <BrowserRouter>
         <Router/>
-      </BrowserRouter>
     </div>
   )
 }
